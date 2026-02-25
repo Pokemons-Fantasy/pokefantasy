59f929cbf7cefbbc0b1a5bfcf1f7c0739d316ed6
Adrian R <villudo@gmail.com>
2026-02-25 22:20:06 +0100

    [SMS-17] Endpoint for create users and login users

---
+++ src/api-rest/src/main/java/com/villu/pokefantasy/PkmnController.java
@@ -0,0 +1,27 @@
+package com.villu.pokefantasy;
+
+import com.villu.pokefantasy.commands.getPokemon.GetPokemonRest;
+import com.villu.pokefantasy.commands.users.saveUser.SaveUser;
+import com.villu.pokefantasy.request.user.UserRequest;
+import com.villu.pokefantasy.response.PokemonsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+public class PkmnController {
+
+    @Autowired
+    private GetPokemonRest getPokemonRest;
+
+
+    @GetMapping("/pokemons/{id}")
+    public ResponseEntity<PokemonsResponse> getPokemons(@PathVariable int id) throws Exception {
+        return ResponseEntity.ok(getPokemonRest.getPokemonById(id));
+    }
+
+
+
+
+}
--- src/api-rest/src/main/java/com/villu/pokefantasy/UserController.java
+++ src/api-rest/src/main/java/com/villu/pokefantasy/UserController.java
@@ -1,27 +1,20 @@
 package com.villu.pokefantasy;
 
-import com.villu.pokefantasy.commands.getPokemon.GetPokemonRest;
 import com.villu.pokefantasy.commands.users.saveUser.SaveUser;
 import com.villu.pokefantasy.request.user.UserRequest;
-import com.villu.pokefantasy.response.PokemonsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1")
-public class Controller {
-
-    @Autowired
-    private GetPokemonRest getPokemonRest;
+public class UserController {
 
     @Autowired
     private SaveUser saveUser;
-
-    @GetMapping("/pokemons/{id}")
-    public ResponseEntity<PokemonsResponse> getPokemons(@PathVariable int id) throws Exception {
-        return ResponseEntity.ok(getPokemonRest.getPokemonById(id));
-    }
 
     @PostMapping("/user")
     public ResponseEntity<Void> createUser(@RequestBody UserRequest user) {
